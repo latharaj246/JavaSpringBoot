7f5cb5bd05174985c7cf3018ac47f45488c05ca2
Latha <latha.r@mpokket.com>
2024-11-28 20:47:39 +0530

    @Autowire annotation

--- src/main/java/com/example/autowire/annotation/Componentscan/App.java
+++ src/main/java/com/example/autowire/annotation/Componentscan/App.java
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
         Employee employee = context.getBean("employee", Employee.class);
+        Manager manager = context.getBean("manager", Manager.class);
 
         System.out.println(employee.toString());
+        System.out.println(manager.toString());
 
     }
 }
--- src/main/java/com/example/autowire/annotation/Componentscan/Manager.java
+++ src/main/java/com/example/autowire/annotation/Componentscan/Manager.java
@@ -1,6 +1,7 @@
 package com.example.autowire.annotation.Componentscan;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,10 +9,16 @@ public class Manager {
 
     private Employee employee;
 
+   // constructor injection (recommended)
     @Autowired
     public Manager(Employee employee) {
         this.employee = employee;
     }
+
+    //field injection
+    @Autowired
+        @Qualifier("employee") // used when u have multiple same bean name (to specify which bean name)
+   private Manager manager;
 
     @Override
     public String toString() {
