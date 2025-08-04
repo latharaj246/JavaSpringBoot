e2b49fcbf7fc86807358a249e33285028bc5fbb2
latha <latharaj246@gmail.com>
2025-08-04 08:18:32 +0530

    model napping

--- src/main/java/com/ecommerce/service/CategoryService.java
+++ src/main/java/com/ecommerce/service/CategoryService.java
@@ -1,15 +1,15 @@
 package com.ecommerce.service;
 
-import com.ecommerce.model.Category;
 import com.ecommerce.payload.CategoryDTO;
 import com.ecommerce.payload.CategoryResponse;
 
 public interface CategoryService {
+
     CategoryResponse getAllCategories();
 
-    CategoryDTO createCategory(Category category);
+    CategoryDTO createCategory(CategoryDTO categoryDTO);
 
-    String deleteCategory(Long categoryId);
+    CategoryDTO deleteCategory(Long categoryId);
 
-    Category updateCategory(Category category, Long categoryId);
+    CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId);
 }
